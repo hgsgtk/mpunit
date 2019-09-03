3a282fc12c55bc8ba02eff2d553074a37e75ae38
hgsgtk <hgsgtk@gmail.com>
2019-09-04 02:53:52 +0900

    using assert

--- test.php
+++ test.php
@@ -2,12 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-(2 === 1 + 1) or die("LINE: " . __LINE__);
-(3.0 === 1.5 + 1.5) or die("LINE: " . __LINE__);
-$date = date('Y-m-d', 4502304000);
-("2112-09-13" === $date) or die("LINE: " . __LINE__);
+assert(2 === 1 + 1);
+assert(3 === 1.5 + 1.5);
+assert('2112-09-13' === date('Y-m-d', 4503168000));
 
 $a = [1, 2, 3];
-(1 == array_shift($a)) or die("LINE: " . __LINE__);
-
-echo "ok" . PHP_EOL;
+assert(1 === array_shift($a));
