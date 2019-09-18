b687d7c2db91e840011fecaec508d37ddbbf424c
hgsgtk <hgsgtk@gmail.com>
2019-09-18 17:19:11 +0900

    add example

---
+++ bin/fizzbuzz1_test.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . "/fizzbuzz.php";
+
+assert('1' === $actual = fizzbuzz(2), 'テスト失敗');
+assert('Fizz' === $actual = fizzbuzz(3));
+assert('Buzz' === $actual = fizzbuzz(5));
+assert('FizzBuzz' === $actual = fizzbuzz(11));
