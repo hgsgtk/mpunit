3eb0f0ffb630a41968efa5fa5626d17e481d66ab
hgsgtk <hgsgtk@gmail.com>
2019-09-20 00:35:15 +0900

    create Command class and remove function from bin file

---
+++ src/Command.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+
+
+namespace MPUnit;
+
+use ReflectionClass;
+
+/**
+ * Class Command
+ * @package MPUnit
+ */
+final class Command
+{
+    /**
+     * @param string $testDir
+     * @return int
+     */
+    public function run(string $testDir): int
+    {
+        chdir($testDir);
+
+        // Collect test files
+        $testFiles = glob('*Test.php');
+        foreach ($testFiles as $testFile) {
+            require_once $testFile;
+        }
+
+        // Collect test classes
+        $testClassNames = array_filter(
+            get_declared_classes(),
+            function ($className) {
+                $ref = new ReflectionClass($className);
+                if (!$ref->isUserDefined()) {
+                    return false;
+                }
+                $parent = $ref->getParentClass();
+                if (!$parent) {
+                    return false;
+                }
+                if ($parent->getName() !== 'MPUnit\TestCase') {
+                    return false;
+                }
+                return true;
+            }
+        );
+
+        $testResult = new TestResult();
+
+        foreach ($testClassNames as $testClassName) {
+            $testResult->incrementTestsCount();
+
+            $testClassRef = new ReflectionClass($testClassName);
+
+            $testMethods = array_filter(
+                array_column($testClassRef->getMethods(\ReflectionMethod::IS_PUBLIC), 'name'),
+                function ($methodName) {
+                    return 'test' === substr($methodName, 0, 4);
+                }
+            );
+
+            foreach ($testMethods as $testMethod) {
+                try {
+                    $test = new $testClassName();
+                    $test->$testMethod();
+                    $testResult->addSuccess();
+                    echo '.';
+                } catch (\AssertionError $e) {
+                    $testResult->addFailure(
+                        $e->getFile(),
+                        $testClassName,
+                        $testMethod,
+                        $e->getLine(),
+                        $e->getMessage()
+                    );
+                    echo 'F';
+                }
+            }
+        }
+
+        echo PHP_EOL . PHP_EOL;
+        return $testResult->endTest();
+    }
+}
