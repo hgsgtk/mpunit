7391861b218939db97c1e7adc26bed5229bc5ad7
hgsgtk <hgsgtk@gmail.com>
2019-09-19 19:44:29 +0900

    can directory by passing argument

--- example/tests/fizzbuzz3_test.php
+++ example/tests/fizzbuzz3_test.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . "/fizzbuzz.php";
+require_once __DIR__ . "/../src/fizzbuzz.php";
 
 assert('1' === $actual = fizzbuzz(1), 'テスト失敗');
 assert('Fizz' === $actual = fizzbuzz(3));
