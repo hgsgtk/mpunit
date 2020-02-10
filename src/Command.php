fc75a30e7b84cbdba15b9f3a34ac713caddd947e
hgsgtk <hgsgtk@gmail.com>
2020-02-10 22:00:19 +0900

    TestCase implements "standard test interface"

--- src/Command.php
+++ src/Command.php
@@ -45,89 +45,12 @@ final class Command
         $testResult = new TestResult();
 
         foreach ($testClassNames as $testClassName) {
-            // Fixme count test case automatically
-            $testResult->incrementTestsCount();
-
-            // Collect test methods
-            $testClassRef = new ReflectionClass($testClassName);
-            $testMethods = array_filter(
-                array_column($testClassRef->getMethods(\ReflectionMethod::IS_PUBLIC), 'name'),
-                function ($methodName) {
-                    return 'test' === substr($methodName, 0, 4);
-                }
-            );
-
-            // Execute test methods
-            foreach ($testMethods as $testMethod) {
-                    // Search doc comment and provider
-                    $refTestMethod = $testClassRef->getMethod($testMethod);
-                    $docComment = $refTestMethod->getDocComment();
-                if ($docComment &&
-                        preg_match_all(self::REGEX_DATA_PROVIDER, $docComment, $matches)
-                        /**
-                         * ..array(2) {
-                         *   [0]=>
-                         *   array(1) {
-                         *     [0]=>
-                         *     string(30) "@dataProvider providerFizzBuzz"
-                         *   }
-                         *   [1]=>
-                         *     array(1) {
-                         *     [0]=>
-                         *     string(16) "providerFizzBuzz"
-                         *   }
-                         * }
-                         * ....
-                         */
-                    ) {
-                    $providerFunc = $matches[1][0];
-                    $providedArgs = (new $testClassName())->$providerFunc();
-                } else {
-                    $providedArgs = [];
-                }
-
-                if (count($providedArgs) !== 0) {
-                    foreach ($providedArgs as $args) {
-                        $testResult = $this->doRunTest($testClassName, $testMethod, $args, $testResult);
-                    }
-                } else {
-                    $testResult = $this->doRunTest($testClassName, $testMethod, $providedArgs, $testResult);
-                }
-            }
+            /** @var Test $testCase */
+            $testCase = new $testClassName();
+            $testResult = $testCase->run($testResult);
         }
 
         echo PHP_EOL . PHP_EOL;
         return $testResult->endTest();
-    }
-
-    /**
-     * @param $testClassName
-     * @param $testMethod
-     * @param $args
-     * @param TestResult $testResult
-     * @return TestResult
-     */
-    private function doRunTest($testClassName, $testMethod, $args, TestResult $testResult): TestResult
-    {
-        try {
-            /** @var TestCase $sut */
-            $sut = new $testClassName();
-            $sut->setUp();
-            $sut->$testMethod(...$args);
-            $sut->tearDown();
-            $testResult->addPass(new Pass());
-            echo '.';
-        } catch (\AssertionError $e) {
-            $testResult->addFailure(
-                new Failure(
-                    $testClassName,
-                    $testMethod,
-                    $e->getMessage(),
-                    $e->getTraceAsString()
-                )
-            );
-            echo 'F';
-        }
-        return $testResult;
     }
 }
--- src/TestCase.php
+++ src/TestCase.php
@@ -3,14 +3,81 @@
 
 namespace MPUnit;
 
+use ReflectionClass;
+use ReflectionMethod;
+
 /**
  * Class TestCase
  *
  * @package MPUnit
  */
-abstract class TestCase
+abstract class TestCase implements Test
 {
     use Assert;
+
+    private const REGEX_DATA_PROVIDER = '/@dataProvider\s+([a-zA-Z0-9._:-\\\\x7f-\xff]+)/';
+
+    /**
+     * @param TestResult $result
+     * @return TestResult
+     * @throws \ReflectionException
+     */
+    public function run(TestResult $result): TestResult
+    {
+        $class = get_called_class();
+        $classRef = new ReflectionClass($class);
+        $testMethods = array_filter(
+            array_column($classRef->getMethods(ReflectionMethod::IS_PUBLIC), 'name'),
+            function ($methodName) {
+                return 'test' === substr($methodName, 0, 4);
+            }
+        );
+
+        foreach ($testMethods as $testMethod) {
+            // Fixme support provider function in other classes
+            $methodRef = $classRef->getMethod($testMethod);
+            $methodDoc = $methodRef->getDocComment();
+            if ($methodDoc &&
+                preg_match_all(self::REGEX_DATA_PROVIDER, $methodDoc, $matches)
+                /**
+                 * ..array(2) {
+                 *   [0]=>
+                 *   array(1) {
+                 *     [0]=>
+                 *     string(30) "@dataProvider providerFizzBuzz"
+                 *   }
+                 *   [1]=>
+                 *     array(1) {
+                 *     [0]=>
+                 *     string(16) "providerFizzBuzz"
+                 *   }
+                 * }
+                 * ....
+                 */
+            ) {
+                $providerFunc = $matches[1][0];
+                $providedArgs = $this->$providerFunc();
+            } else {
+                $providedArgs = [[]]; // one element
+            }
+
+            foreach ($providedArgs as $args) {
+                $result = $this->doRunTest($testMethod, $args, $result);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * TestCase object count
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return 1;
+    }
 
     /**
      * @fixme make protected
@@ -26,5 +93,41 @@ abstract class TestCase
     public function tearDown(): void
     {
         // Implement if you needed
+    }
+
+    /**
+     * @param $testClassName
+     * @param $testMethod
+     * @param $args
+     * @param TestResult $testResult
+     * @return TestResult
+     */
+    private function doRunTest($testMethod, $args, TestResult $testResult): TestResult
+    {
+        $testResult->incrementTestsCount();
+
+        $className = get_called_class();
+        try {
+            /** @var TestCase $testClass */
+            $testClass = new $className();
+            $testClass->setUp();
+            $testClass->$testMethod(...$args);
+            $testClass->tearDown();
+            $testResult->addPass(new Pass());
+            // Fixme print stdout outside TestCase
+            echo '.';
+        } catch (\AssertionError $e) {
+            $testResult->addFailure(
+                new Failure(
+                    $className,
+                    $testMethod,
+                    $e->getMessage(),
+                    $e->getTraceAsString()
+                )
+            );
+            // Fixme print stdout outside TestCase
+            echo 'F';
+        }
+        return $testResult;
     }
 }
